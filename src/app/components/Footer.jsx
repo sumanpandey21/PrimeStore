0b326d728faf77f221369878d9131abfebebdc36
sumanpandey21 <pandeysuman9988@gamil.com>
2025-09-03 09:12:26 +0545

    Footer fix

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -9,7 +9,7 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white pt-10">
+    <footer className=" w-full bg-black text-white pt-10">
       <div className="max-w-7xl mx-auto px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
         <div>
           <h2 className="text-xl font-bold mb-4">PrimeStore</h2>
@@ -33,44 +33,46 @@ export default function Footer() {
         <div>
           <h2 className="text-xl font-bold mb-4">Support</h2>
           <p className="mb-2">support@primestore.com</p>
-          <p className="px-0.5">+977 9800000254</p>
+          <p className="mb-2">+977 9800000254</p>
         </div>
 
         <div>
           <h2 className="text-xl font-bold mb-4">Account</h2>
-          <ul className="space-y-2">
+          <ul className="flex flex-col space-y-2">
             <Link href={"/myaccount"}>
-              <li
-                className="relative inline-block cursor-pointer 
-                  after:content-[''] after:absolute after:left-0 after:bottom-0 
-                  after:w-0 after:h-[2px] after:bg-orange-500 
-                  after:transition-all after:duration-300 hover:after:w-full"
-              >
-                My Account
-              </li>
+              <li className="footer-link ">My Account</li>
             </Link>
-            <li className="hover:text-gray-300 cursor-pointer">
-              Login / Register
-            </li>
-            <li className="hover:text-gray-300 cursor-pointer">Cart</li>
-            <li className="hover:text-gray-300 cursor-pointer">Wishlist</li>
-            <li className="hover:text-gray-300 cursor-pointer">Shop</li>
+
+            <Link href={""}>
+              <li className="footer-link">Login / Register</li>
+            </Link>
+            <Link href={""}>
+              <li className="footer-link">Cart</li>
+            </Link>
+            <Link href={""}>
+              <li className="footer-link">Wishlist</li>
+            </Link>
+            <Link href={""}>
+              <li className="footer-link">Shop</li>
+            </Link>
           </ul>
         </div>
 
         <div>
           <h2 className="text-xl font-bold mb-4">Quick Links</h2>
-          <ul className="space-y-2">
-            <li className="hover:text-gray-300 cursor-pointer">
-              Privacy Policy
-            </li>
-            <li className="hover:text-gray-300 cursor-pointer">Terms of Use</li>
+          <ul className="flex flex-col space-y-2">
+            <Link href={"#"}>
+              <li className="footer-link ">Privacy Policy</li>
+            </Link>
+            <Link href={"#"}>
+              <li className="footer-link ">Terms of Use</li>
+            </Link>
 
             <Link href={"/about"}>
-              <li className="hover:text-gray-300 cursor-pointer">About</li>
+              <li className="footer-link ">About</li>
             </Link>
             <Link href={"/contact"}>
-              <li className="hover:text-gray-300 cursor-pointer">Contact</li>
+              <li className="footer-link ">Contact</li>
             </Link>
           </ul>
         </div>
