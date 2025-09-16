13342ba163a5f18bc78c7d3f73deb1fefb154de0
sumanpandey21 <pandeysuman9988@gamil.com>
2025-09-16 16:46:19 +0545

    Add postgres database configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'primestoredb',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',   # or 127.0.0.1
+        'PORT': '5432',        # default PostgreSQL port
     }
 }
 
