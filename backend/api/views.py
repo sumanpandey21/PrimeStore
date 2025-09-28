4919f34a0212b13c8e6f38c92e6a6591a513e038
sumanpandey21 <pandeysuman9988@gamil.com>
2025-09-28 20:36:35 +0545

    Make views

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,13 +4,36 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
-from .serializers import RegisterSerializer
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.core.mail import send_mail
 from django.conf import settings
 from rest_framework.views import APIView
+from .serializers import (
+    RegisterSerializer,
+    UserSerilizer,
+    ProductSerilizer,
+    CartSerilizer,
+    CheckoutSerilizer,
+    OrderSerilizer,
+    CategorySerilizer,
+    RatingSerilizer,
+    WishlistSerilizer,
+    CancellationSerilizer,
+)
+from .models import (
+    Product,
+    Cart,
+    Checkout,
+    Category,
+    Cancellation,
+    Order,
+    Wishlist,
+    Rating,
+    User,
+)
+
 
 class RegisterViewSet(viewsets.ViewSet):
     def create(self, request):
@@ -34,12 +57,16 @@ class RegisterViewSet(viewsets.ViewSet):
                 fail_silently=False,
             )
 
-            return Response({
-                "message": "User registered. Please check your email to confirm.",
-                "status": True,
-            }, status=status.HTTP_201_CREATED)
+            return Response(
+                {
+                    "message": "User registered. Please check your email to confirm.",
+                    "status": True,
+                },
+                status=status.HTTP_201_CREATED,
+            )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ActivateUser(APIView):
     def get(self, request, uidb64, token):
@@ -56,23 +83,66 @@ class ActivateUser(APIView):
         else:
             return Response({"error": "Invalid activation link"}, status=400)
 
+
 class LoginViewSet(viewsets.ViewSet):
     def create(self, request):
         data = request.data
         username = data.get("username")
         password = data.get("password")
-        user = User.objects.filter(username = username).first()
+        user = User.objects.filter(username=username).first()
         if user and user.check_password(password):
             refresh = RefreshToken.for_user(user)
-            return Response({
-                "access_token" : str(refresh.access_token),
-                "refresh_token":str(refresh)
-            })
-        return Response({"message": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {
+                    "access_token": str(refresh.access_token),
+                    "refresh_token": str(refresh),
+                }
+            )
+        return Response(
+            {"message": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerilizer
+    queryset = User.objects.all()
 
 
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerilizer
+    queryset = Product.objects.all()
 
 
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerilizer
+    queryset = Category.objects.all()
 
+
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerilizer
+    queryset = Cart.objects.all()
+
+
+class WishlistViewSet(viewsets.ModelViewSet):
+    serializer_class = WishlistSerilizer
+    queryset = Wishlist.objects.all()
+
+
+class CheckoutViewSet(viewsets.ModelViewSet):
+    serializer_class = CheckoutSerilizer
+    queryset = Checkout.objects.all()
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerilizer
+    queryset = Order.objects.all()
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    serializer_class = RatingSerilizer
+    queryset = Rating.objects.all()
+
+
+class CancellationViewSet(viewsets.ModelViewSet):
+    serializer_class = CancellationSerilizer
+    queryset = Cancellation.objects.all()
